0184e7da53d604c0339984f70750b80f6571cbc8
allenlin90 <ht016746@gmail.com>
2021-09-11 22:20:18 +0700

    finish History view

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,126 +1,156 @@
 <template>
-  <v-container fluid>
-    <v-sheet tile height="54" class="d-flex">
-      <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-select
-        v-model="type"
-        :items="types"
-        dense
-        outlined
-        hide-details
-        class="ma-2"
-        label="type"
-      ></v-select>
-      <v-select
-        v-model="mode"
-        :items="modes"
-        dense
-        outlined
-        hide-details
-        label="event-overlap-mode"
-        class="ma-2"
-      ></v-select>
-      <v-select
-        v-model="weekday"
-        :items="weekdays"
-        dense
-        outlined
-        hide-details
-        label="weekdays"
-        class="ma-2"
-      ></v-select>
-      <v-spacer></v-spacer>
-      <v-btn icon class="ma-2" @click="$refs.calendar.next()">
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-sheet>
-    <v-sheet>
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :weekdays="weekday"
-        :type="type"
-        :events="events"
-        :event-overlap-mode="mode"
-        :event-overlap-threshold="30"
-        :event-color="getEventColor"
-        @change="getEvents"
-      ></v-calendar>
-    </v-sheet>
+  <v-container fluid :fill-height="!hasRecords" class="align-start">
+    <v-row dense :justify="hasRecords ? 'between' : 'center'">
+      <v-col cols="6"
+        ><h1 :class="{ 'text-center': !hasRecords, 'text-start': hasRecords }">
+          History
+        </h1></v-col
+      >
+      <v-col v-if="hasRecords" cols="6" class="d-flex justify-end align-center">
+        <v-btn dark color="red" @click="removeAllRecords"
+          >Clear All Records</v-btn
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-if="!hasRecords" cols="12" class="text-center">
+        <h1>You have no records yet...</h1>
+        <router-link to="/news">Read some News</router-link>
+      </v-col>
+      <v-col v-else>
+        <v-card>
+          <v-card-text>
+            <v-list>
+              <v-list-group v-for="date in dates" :key="date">
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title
+                      ><h3 class="font-weight-bold">
+                        {{ date }}
+                      </h3></v-list-item-title
+                    >
+                  </v-list-item-content>
+                </template>
+                <v-list-item
+                  v-for="(record, index) in recordList(date)"
+                  :key="date + index"
+                >
+                  <v-row>
+                    <v-col cols="3"
+                      >{{ getTimestamp(record.timestamp) }}
+                    </v-col>
+                    <v-col cols="8">{{ record.to }} </v-col>
+                    <v-col cols="1" class="d-flex justify-end">
+                      <v-icon
+                        class="d-block"
+                        @click="removeRecord(date, record.timestamp)"
+                        outlined
+                        >mdi-trash-can-outline</v-icon
+                      >
+                    </v-col>
+                  </v-row>
+                </v-list-item>
+              </v-list-group>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
+  name: 'History',
   data: () => ({
-    type: 'month',
-    types: ['month', 'week', 'day', '4day'],
-    mode: 'stack',
-    modes: ['stack', 'column'],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
-    ],
-    value: '',
-    events: [],
-    colors: [
-      'blue',
-      'indigo',
-      'deep-purple',
-      'cyan',
-      'green',
-      'orange',
-      'grey darken-1',
-    ],
-    names: [
-      'Meeting',
-      'Holiday',
-      'PTO',
-      'Travel',
-      'Event',
-      'Birthday',
-      'Conference',
-      'Party',
-    ],
+    records: null,
   }),
-  methods: {
-    getEvents({ start, end }) {
-      const events = [];
-
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
-
-      for (let i = 0; i < eventCount; i += 1) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        });
+  computed: {
+    hasRecords() {
+      if (this.records) {
+        return true;
       }
-
-      this.events = events;
+      return false;
     },
-    getEventColor(event) {
-      return event.color;
+    dates() {
+      if (this.records) {
+        return Object.keys(this.records).reverse();
+      }
+      return [];
     },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
+  },
+  methods: {
+    removeRecord(date, timestamp) {
+      this.records[date] = this.records[date].filter(
+        (item) => item.timestamp !== timestamp,
+      );
     },
+    removeAllRecords() {
+      this.records = null;
+    },
+    getTimestamp(timestamp) {
+      const seconds = Date.parse(timestamp);
+      // const year = new Date(seconds).getFullYear();
+      // const month = new Date(seconds).getMonth() + 1;
+      // const date = new Date(seconds).getDate();
+      let hours = new Date(seconds).getHours();
+      if (String(hours).length === 1) {
+        hours = `0${hours}`;
+      }
+      let minutes = new Date(seconds).getMinutes();
+      if (String(minutes).length === 1) {
+        minutes = `0${minutes}`;
+      }
+      // return `${year}/${month}/${date} - ${hours}:${minutes}`;
+      return `${hours}:${minutes}`;
+    },
+    getDate(timestamp) {
+      const seconds = Date.parse(timestamp);
+      if (!Number.isNaN(seconds) && !!seconds) {
+        const dateObj = new Date(seconds);
+        const year = dateObj.getFullYear();
+        const month = dateObj.getMonth();
+        const date = dateObj.getDate();
+        return {
+          dateStr: `${year}-${month + 1}-${date}`,
+        };
+      }
+      return false;
+    },
+    recordList(date) {
+      // localStorage.removeItem('hitstory');
+      return this.records[date];
+    },
+  },
+  beforeMount() {
+    const rawRecords = localStorage.getItem('history');
+    if (rawRecords) {
+      const records = JSON.parse(rawRecords);
+      const collection = {};
+      records.forEach((record) => {
+        const { to, from, timestamp } = record;
+        const { dateStr } = this.getDate(timestamp);
+        if (!(dateStr in collection)) {
+          collection[dateStr] = [];
+        }
+        collection[dateStr].push({ timestamp, to, from });
+      });
+      const keys = Object.keys(collection);
+      keys.forEach((key) => {
+        collection[key] = collection[key].reverse();
+      });
+      this.records = collection;
+    }
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.text-center {
+  text-align: center;
+}
+
+.text-start {
+  text-align: start;
+}
+</style>
