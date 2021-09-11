2b60196917e9cc285b576c42d26e140d51228d74
allenlin90 <ht016746@gmail.com>
2021-09-12 01:18:11 +0700

    finish search input and connect to vuex

--- src/views/NewsGrid.vue
+++ src/views/NewsGrid.vue
@@ -1,112 +1,145 @@
 <template>
   <v-container style="max-width:1264px">
     <v-row justify="space-between">
-      <v-col cols="6" align-self="center">
+      <v-col cols="6" sm="3" align-self="center" order="1">
         <h1>HEADLINES</h1>
       </v-col>
-      <v-col cols="6" align-self="center">
-        <v-btn class="d-block ml-auto" color="primary">
-          FILTER <v-icon>mdi-filter-outline</v-icon>
-        </v-btn>
+      <v-col cols="12" sm="7" align-self="center" order="3">
+        <v-text-field
+          v-model.trim="search"
+          outlined
+          hide-details
+          label="Search"
+          clearable
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6" sm="2" align-self="center" order="2" order-sm="4">
+        <filter-pop v-if="activeFilter" :rotate="rotate"> </filter-pop>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12" sm="6" md="4" v-for="post in news" :key="post.newsId">
-        <news-card
-          :title="post.title"
-          :subtitle="post.subtitle"
-          :img="post.img"
-          :content="post.content"
-          :newsId="post.newsId"
-        ></news-card>
-      </v-col>
+    <v-row v-if="!!articlesToRender.length">
       <v-col
         cols="12"
         sm="6"
         md="4"
-        v-for="(post, index) in articles"
+        v-for="(post, index) in articlesToRender"
         :key="`n${index}`"
       >
         <news-card
           :title="post.title"
           :subtitle="parseDateString(post.publishedAt)"
           :img="post.urlToImage"
-          :content="post.discription"
+          :content="
+            post.content
+              ? post.content
+              : post.description
+              ? post.description
+              : ''
+          "
           :newsId="`n${index}`"
         ></news-card>
       </v-col>
     </v-row>
+    <v-row v-else>
+      <v-col>
+        <h1>Oops... there is nothing to show</h1>
+      </v-col>
+    </v-row>
+    <v-btn
+      v-show="fab"
+      :fab="fab"
+      bottom
+      right
+      fixed
+      @click="scrollTop"
+      v-scroll="onScroll"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
   </v-container>
 </template>
 
 <script>
-import NewsCard from '../components/newsGrid/NewsCard.vue';
+import { mapGetters, mapActions } from 'vuex';
+import NewsCard from '@/components/newsGrid/NewsCard.vue';
+import FilterPop from '@/components/newsGrid/FilterPop.vue';
 
 export default {
-  components: { NewsCard },
+  components: { NewsCard, FilterPop },
   name: 'Home',
   data: () => ({
-    news: [
-      {
-        newsId: '1',
-        title: 'Thailand Today',
-        subtitle: '2021/09/30',
-        img: 'https://source.unsplash.com/daily',
-        content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
-      },
-      {
-        newsId: '2',
-        title: 'Natural Call',
-        subtitle: '2021/09/30',
-        img: 'https://source.unsplash.com/1600x900/?nature,water',
-        content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
-      },
-      {
-        newsId: '3',
-        title: 'Adventure',
-        subtitle: '2021/09/30',
-        img: 'https://source.unsplash.com/1600x900/?nature',
-        content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
-      },
-      {
-        newsId: '4',
-        title: 'Motors',
-        subtitle: '2021/09/30',
-        img: 'https://source.unsplash.com/1600x900/?car',
-        content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
-      },
-      {
-        newsId: '5',
-        title: 'Food',
-        subtitle: '2021/09/30',
-        img: 'https://source.unsplash.com/1600x900/?food',
-        content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
-      },
-    ],
+    rotate: false,
+    fab: false,
+    keyword: '',
+    search: '',
+    timer: null,
   }),
-  computed: {
-    articles() {
-      return this.$store.getters['news/articles'];
+  watch: {
+    search(value) {
+      if (value) {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        this.timer = setTimeout(() => {
+          this.searchNews(value);
+        }, 500);
+      }
     },
+  },
+  computed: {
+    articlesToRender() {
+      if (this.search) {
+        return this.searchResult;
+      }
+      return this.articles;
+    },
+    activeFilter() {
+      if (this.sources.length) {
+        return true;
+      }
+      return false;
+    },
+    ...mapGetters('news', ['articles', 'sources', 'searchResult']),
   },
   methods: {
     parseDateString(dateStr) {
       const time = new Date(Date.parse(dateStr));
       return `${time.getFullYear()}/${time.getMonth() + 1}/${time.getDate()}`;
     },
+    scrollTop() {
+      this.$vuetify.goTo(0, {
+        easing: 'easeInCubic',
+        duration: '1000',
+      });
+    },
+    onScroll(e) {
+      if (typeof window === 'undefined') return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    ...mapActions('news', [
+      'getArticles',
+      'getSources',
+      'searchNews',
+      'errorRequest',
+    ]),
   },
   async beforeCreate() {
     this.$store.dispatch('overlay');
   },
   async mounted() {
-    await this.$store.dispatch('news/articles');
-    await this.$store.dispatch('news/sources');
+    if (!this.articles.legnth) {
+      await this.getArticles();
+    }
     this.$store.dispatch('overlay');
+
+    this.rotate = true;
+    if (!this.sources.length) {
+      await this.getSources();
+    }
+    this.rotate = false;
+
+    // await this.errorRequest();
   },
 };
 </script>
