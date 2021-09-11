a543da9a2b8f14dc950e266fab248179476261dc
allenlin90 <ht016746@gmail.com>
2021-09-11 22:21:00 +0700

    prepare local data for API downtime

---
+++ src/assets/newsPlaceholder.js
@@ -0,0 +1,44 @@
+export default {
+  news: [
+    {
+      newsId: '1',
+      title: 'Thailand Today',
+      subtitle: '2021/09/30',
+      img: 'https://source.unsplash.com/daily',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
+    },
+    {
+      newsId: '2',
+      title: 'Natural Call',
+      subtitle: '2021/09/30',
+      img: 'https://source.unsplash.com/1600x900/?nature,water',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
+    },
+    {
+      newsId: '3',
+      title: 'Adventure',
+      subtitle: '2021/09/30',
+      img: 'https://source.unsplash.com/1600x900/?nature',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
+    },
+    {
+      newsId: '4',
+      title: 'Motors',
+      subtitle: '2021/09/30',
+      img: 'https://source.unsplash.com/1600x900/?car',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
+    },
+    {
+      newsId: '5',
+      title: 'Food',
+      subtitle: '2021/09/30',
+      img: 'https://source.unsplash.com/1600x900/?food',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium saepe voluptate dolor ea dolores. olestias nam harum animi odio porro?',
+    },
+  ],
+};
