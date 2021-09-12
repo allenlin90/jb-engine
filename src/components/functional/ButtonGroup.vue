08ac1636bd2681d0974dc91c88ba427928f7921c
allenlin90 <ht016746@gmail.com>
2021-09-12 16:39:21 +0700

    add functional component 'ButtonGroup'

---
+++ src/components/functional/ButtonGroup.vue
@@ -0,0 +1,33 @@
+<template functional>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <a
+      class="btn btn-secondary text-white"
+      :href="props.link"
+      target="_blank"
+      @click="listeners['external']"
+    >
+      <slot name="link">
+        {{ props.link }}
+      </slot>
+    </a>
+    <button class="btn btn-secondary" @click="listeners['edit']">
+      <slot>Edit</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonGroup',
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+</style>
